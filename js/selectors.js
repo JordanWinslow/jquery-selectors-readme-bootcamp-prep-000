7999a9cb02bcabdd0b057db8bd84e46a4862692d
Jordan Winslow <admin@jordanwinslow.me>
2019-08-10 01:28:30 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,17 @@
 // declare your functions here...
+
+const paragraphSelector = () => {
+  return $("p")
+}
+
+const lastImageSelector = () => {
+  return $("img:last")
+}
+
+const ninjaBabySelector = () => {
+  return $("div .pics")
+}
+
+const firstListItem = () => {
+  
+}
